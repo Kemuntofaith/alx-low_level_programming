e44f3ebbbaa834dd724af80ebed7ceae1f765381
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-09 05:10:41 -0700

    commit0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * * main - tests function that prints if integer is positive or negative
- * * Return: 0
- * */
+ * main - test functions that print if integeris positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
