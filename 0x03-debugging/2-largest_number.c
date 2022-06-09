5270981c881aacc2d95934e9860b9ffc77e24845
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-09 06:45:49 -0700

    commit3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,11 +18,11 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b > a && b > c)
 	{
-		largest = a;
+		largest = b;
 	}
 	else
 	{
-		largest = a;
+		largest = c;
 	}
 	return (largest);
 }
