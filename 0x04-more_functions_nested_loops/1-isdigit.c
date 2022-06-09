b7e26dabd628728fa7e89667645a85d0e4570de4
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-09 12:23:23 -0700

    commit 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * _isdigit - writes the character c to stdout
+ * _isdigit - checks for a digit (0 through 9)
  * @c: the character being checked
  * Return: 1 if digit else 0
  */
