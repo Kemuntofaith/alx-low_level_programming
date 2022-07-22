8bc56dd7b4a73d4fad5f2ee9ae7114675bbc8a55
Kemuntofaith <kemuntofaith20@gmail.com>
2022-07-22 00:50:33 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,23 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) != 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((i % 5) == 0 && (i % 3) != 0)
-		{
-			printf("Buzz ");
-		}
-		else if ((i % 3) == 0 && (i % 5) == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
 			printf("%d", i);
 		}
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
